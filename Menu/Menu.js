52f34c838f18dab0f32945f64eff64d21a39bc52
Tom Hessburg <tomhessburg@Toms-MacBook-Pro.local>
2019-01-30 16:34:31 -0500

    Added functionality to expand button, boxes animate in and out

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,6 +1,8 @@
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
+  
+
   menu.classList.toggle("menu--open");
 }
 
@@ -15,3 +17,4 @@ console.log(menuButton);
 
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener("click", () => toggleMenu());
+
